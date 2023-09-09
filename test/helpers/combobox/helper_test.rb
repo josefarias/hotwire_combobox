8b97df9e09be77ba9978648e0f3aafb08f791411
Jose Farias <jose@farias.mx>
2023-09-09 14:30:00 -0600

    Clean up helper test

--- test/helpers/combobox/helper_test.rb
+++ test/helpers/combobox/helper_test.rb
@@ -2,28 +2,49 @@ require "test_helper"
 
 class Combobox::HelperTest < ApplicationViewTestCase
   test "passing an input type" do
-    assert_match /<input.* type="search".*/, combobox_tag(:foo, type: :search)
+    tag = combobox_tag :foo, type: :search
+
+    assert_attrs tag, type: "search"
   end
 
   test "passing an id" do
-    assert_match /<input.* id="foobar".*/, combobox_tag(:foo, id: :foobar)
+    tag = combobox_tag :foo, id: :foobar
+
+    assert_attrs tag, id: "foobar"
   end
 
   test "passing a name" do
-    assert_match /<input.* name="foo".*/, combobox_tag(:foo)
+    tag = combobox_tag :foo
+
+    assert_attrs tag, name: "foo"
   end
 
   test "passing a form builder object" do
     form = ActionView::Helpers::FormBuilder.new :foo, nil, self, {}
-    assert_match /<input.* id="foo_bar".* name="foo\[bar\]".*/, combobox_tag(:bar, form: form)
+    tag = combobox_tag :bar, form: form
+
+    assert_attrs tag, id: "foo_bar", name: "foo[bar]"
   end
 
   test "passing an id overrides form builder" do
     form = ActionView::Helpers::FormBuilder.new :foo, nil, self, {}
-    assert_match /<input.* id="foobar".*/, combobox_tag(:bar, form: form, id: :foobar)
+    tag = combobox_tag :bar, form: form, id: :foobar
+
+    assert_attrs tag, id: "foobar"
   end
 
   test "passing open" do
-    assert_match /<div.* data-combobox-expanded-value="true".*/, combobox_tag(:foo, open: true)
+    tag = combobox_tag :foo, open: true
+
+    assert_attrs tag, tag_name: :div, data_combobox_expanded_value: "true"
   end
+
+  private
+    def assert_attrs(tag, tag_name: :input, **attrs)
+      attrs = attrs.map do |k, v|
+        %Q(#{Regexp.escape(k).dasherize}="#{Regexp.escape(v)}".*)
+      end.join(" ")
+
+      assert_match /<#{tag_name}.* #{attrs}/, tag
+    end
 end
