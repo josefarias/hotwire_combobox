5eacbf5dba870585b10c0adfc65ade8165fd7da0
Jose Farias <jose@37signals.com>
2023-12-11 21:22:51 -0600

    Fix tests

--- test/dummy/app/helpers/application_helper.rb
+++ test/dummy/app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def html_combobox_options(options)
     options.map do |option|
-      HotwireCombobox::Option.new \
+      {
         id: option.id,
-        content: render("comboboxes/state", state: option.content),
-        filterable_as: option.filterable_as,
-        autocompletable_as: option.autocompletable_as
-    end
+        display: option.display,
+        content: render("comboboxes/state", state: option.display)
+      }
+    end.then { |options| combobox_options options }
   end
 end
