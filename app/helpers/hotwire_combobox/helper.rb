082e6cef85240dd263a31eb4dfc1cd1e22088d02
Jose Farias <jose@37signals.com>
2023-12-16 09:08:10 -0600

    Be more ruby 2.x friendly

--- app/helpers/hotwire_combobox/helper.rb
+++ app/helpers/hotwire_combobox/helper.rb
@@ -9,12 +9,12 @@ module HotwireCombobox
     end
     alias_method :combobox_options, :hw_combobox_options unless bypass_convenience_methods?
 
-    def hw_combobox_option(...)
-      HotwireCombobox::Option.new(...)
+    def hw_combobox_option(*args, **kwargs, &block)
+      HotwireCombobox::Option.new(*args, **kwargs, &block)
     end
 
-    def hw_combobox_tag(...)
-      render "hotwire_combobox/combobox", component: HotwireCombobox::Component.new(...)
+    def hw_combobox_tag(*args, **kwargs, &block)
+      render "hotwire_combobox/combobox", component: HotwireCombobox::Component.new(*args, **kwargs, &block)
     end
     alias_method :combobox_tag, :hw_combobox_tag unless bypass_convenience_methods?
   end
