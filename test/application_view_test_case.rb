aeaf6238cf632e7700b740ccddcd93174a1ed496
Pranav Babu <babu@maindeck.io>
2024-02-11 12:13:44 +0100

    Add mock_form helper

--- test/application_view_test_case.rb
+++ test/application_view_test_case.rb
@@ -10,6 +10,21 @@ class ApplicationViewTestCase < ActionView::TestCase
     assert_match /<#{tag_name}.* #{attrs}/, tag
   end
 
+  def mock_form(form_object: OpenStruct.new)
+    form = OpenStruct.new
+    form.define_singleton_method(:field_id) { |name| "#{name}_id" }
+    form.define_singleton_method(:field_name) { |name| name }
+    form.define_singleton_method(:object) { form_object }
+    form_object.define_singleton_method(:public_send) do |method_name, *args|
+      if form_object.respond_to?(method_name)
+        super(method_name, *args)
+      else
+        raise NoMethodError, "undefined method `#{method_name}` for #{form_object.inspect}"
+      end
+    end
+    form
+  end
+
   private
     def escape_specials(value)
       special_characters = Regexp.union "[]".chars
