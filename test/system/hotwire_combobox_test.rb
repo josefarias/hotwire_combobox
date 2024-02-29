josefarias/hotwire_combobox

commit 1bd6f6831e01bd0b44a8ae93f109d637c935ce91

Fix filtering inconsistencies (#37)
Co-authored-by: Pranav Babu <babu@maindeck.io>
Co-authored-by: Deanout <dehartdean@gmail.com>

app/assets/javascripts/hw_combobox/models/combobox/selection.js
@@ -1,9 +1,10 @@
 import Combobox from "hw_combobox/models/combobox/base"
-import { wrapAroundAccess } from "hw_combobox/helpers"
+import { wrapAroundAccess, nullEvent } from "hw_combobox/helpers"
 
 Combobox.Selection = Base => class extends Base {
   selectOption(event) {
     this._select(event.currentTarget)
+    this.filter(event)
     this.close()
   }
 
@@ -49,9 +50,9 @@ Combobox.Selection = Base => class extends Base {
     if (option) this._commitSelection(option, { selected: false })
   }
 
-  _selectNew(query) {
+  _selectNew() {
     this._resetOptions()
-    this.hiddenFieldTarget.value = query
+    this.hiddenFieldTarget.value = this._actingCombobox.value
     this.hiddenFieldTarget.name = this.nameWhenNewValue
   }
 
@@ -70,7 +71,18 @@ Combobox.Selection = Base => class extends Base {
     }
   }
 
+  _selectFuzzyMatch() {
+    if (this._isFuzzyMatch) {
+      this._select(this._visibleOptionElements[0], { force: true })
+      this.filter(nullEvent)
+    }
+  }
+
   get _hasValueButNoSelection() {
     return this.hiddenFieldTarget.value && !this._selectedOptionElement
+  }
+
+  get _isFuzzyMatch() {
+    return this._isQueried && !!this._visibleOptionElements[0] && !this._isNewOptionWithPotentialMatches
   }
 }

app/assets/javascripts/hw_combobox/models/combobox/toggle.js
@@ -51,9 +51,7 @@ Combobox.Toggle = Base => class extends Base {
   }
 
   _ensureSelection() {
-    if (!this._isValidNewOption(this._actingCombobox.value, { ignoreAutocomplete: true })) {
-      this._select(this._selectedOptionElement, { force: true })
-    }
+    this._selectFuzzyMatch()
   }
 
   _openByFocusing() {

test/system/hotwire_combobox_test.rb
@@ -110,15 +110,15 @@ class HotwireComboboxTest < ApplicationSystemTestCase
 
     open_combobox
 
-    find("#state-field").send_keys("lor", :enter)
+    find("#state-field").send_keys("is", :enter)
     assert_selector "input[aria-expanded=false]"
-    assert_field "state-field", with: "Florida"
-    assert_field "state-field-hw-hidden-field", type: "hidden", with: "FL"
-    assert_no_selector "li[role=option].hw-combobox__option--selected", text: "Florida" # because the list is closed
+    assert_field "state-field", with: "Mississippi"
+    assert_field "state-field-hw-hidden-field", type: "hidden", with: "MS"
+    assert_no_selector "li[role=option].hw-combobox__option--selected", text: "Mississippi" # because the list is closed
 
     find("#state-field").send_keys(:backspace)
     assert_selector "input[aria-expanded=true]"
-    assert_field "state-field", with: "Florid"
+    assert_field "state-field", with: "Mississipp"
     assert_field "state-field-hw-hidden-field", type: "hidden", with: nil
     assert_no_selector "li[role=option].hw-combobox__option--selected"
   end
@@ -128,11 +128,14 @@ class HotwireComboboxTest < ApplicationSystemTestCase
 
     open_combobox
 
-    find("#state-field").send_keys("lor")
-    click_on_edge
+    find("#state-field").send_keys("is")
+    find("#clickable").click
     assert_selector "input[aria-expanded=false]"
-    assert_field "state-field", with: "Florida"
-    assert_field "state-field-hw-hidden-field", type: "hidden", with: "FL"
+    assert_field "state-field", with: "Mississippi"
+    assert_field "state-field-hw-hidden-field", type: "hidden", with: "MS"
+
+    open_combobox
+    assert_selector "li[role=option]", count: 1
   end
 
   test "focusing away locks in the current selection" do
@@ -140,11 +143,14 @@ class HotwireComboboxTest < ApplicationSystemTestCase
 
     open_combobox
 
-    find("#state-field").send_keys("lor")
+    find("#state-field").send_keys("is")
     find("body").send_keys(:tab)
     assert_selector "input[aria-expanded=false]"
-    assert_field "state-field", with: "Florida"
-    assert_field "state-field-hw-hidden-field", type: "hidden", with: "FL"
+    assert_field "state-field", with: "Mississippi"
+    assert_field "state-field-hw-hidden-field", type: "hidden", with: "MS"
+
+    open_combobox
+    assert_selector "li[role=option]", count: 1
   end
 
   test "navigating with the arrow keys" do
@@ -194,6 +200,13 @@ class HotwireComboboxTest < ApplicationSystemTestCase
     assert_selector "input[aria-expanded=false]"
     assert_field "state-field", with: "Florida"
     assert_field "state-field-hw-hidden-field", type: "hidden", with: "FL"
+
+    open_combobox
+    assert_selector "li[role=option]", count: 1
+    find("#state-field").then do |input|
+      "Florida".chars.each { input.send_keys(:backspace) }
+    end
+    assert_selector "li[role=option]", count: State.count
   end
 
   test "combobox with prefilled value" do
@@ -238,7 +251,7 @@ class HotwireComboboxTest < ApplicationSystemTestCase
     open_combobox
 
     assert_no_selector "input[aria-invalid=true]"
-    find("#state-field").send_keys("Flor", :backspace, :enter)
+    find("#state-field").send_keys("Wisconsin", :enter)
     assert_selector "input[aria-invalid=true]"
   end
 
@@ -329,6 +342,47 @@ class HotwireComboboxTest < ApplicationSystemTestCase
 
     new_user = User.last
     assert_nil new_user.home_state
+  end
+
+  test "going back and forth between new and existing options" do
+    visit new_options_path
+
+    open_combobox "movie-field"
+    find("#movie-field").send_keys("The Godfather")
+    assert_text "The Godfather Part II" # wait for async filter
+    find("#movie-field").send_keys(:backspace) # clear autocompleted portion
+    find("body").send_keys(:tab) # ensure selection
+    assert_field "movie-field-hw-hidden-field", type: "hidden", with: "The Godfather"
+    assert_no_selector "li[role=option][aria-selected=true]"
+    assert_selector "input[name='new_movie']", visible: :hidden
+    assert_no_selector "input[name='movie']", visible: :hidden
+
+    open_combobox "movie-field"
+    assert_selector "li[role=option]", count: 2 # Parts II and III
+    find("li[role=option]", text: "The Godfather Part III").click
+    assert_field "movie-field-hw-hidden-field", type: "hidden", with: Movie.find_by_title("The Godfather Part III").id
+    assert_selector "input[name='movie']", visible: :hidden
+    assert_no_selector "input[name='new_movie']", visible: :hidden
+
+    open_combobox "movie-field"
+    assert_selector "li[role=option]", count: 1 # Part III
+    find("#movie-field").send_keys(:backspace)
+    assert_selector "li[role=option]", count: 2 # Parts II and III
+    find("body").send_keys(:tab) # ensure selection
+    assert_field "movie-field-hw-hidden-field", type: "hidden", with: Movie.find_by_title("The Godfather Part II").id
+    assert_selector "input[name='movie']", visible: :hidden
+    assert_no_selector "input[name='new_movie']", visible: :hidden
+
+    open_combobox "movie-field"
+    find("#movie-field").then do |input|
+      "The Godfather Part II".chars.each { input.send_keys(:arrow_right) }
+      " Part II".chars.each { input.send_keys(:backspace) }
+    end
+    find("body").send_keys(:tab) # ensure selection
+    assert_field "movie-field-hw-hidden-field", type: "hidden", with: "The Godfather"
+    assert_no_selector "li[role=option][aria-selected=true]"
+    assert_selector "input[name='new_movie']", visible: :hidden
+    assert_no_selector "input[name='movie']", visible: :hidden
   end
 
   test "inline-only autocomplete" do

app/assets/javascripts/controllers/hw_combobox_controller.js
@@ -10,6 +10,7 @@ const concerns = [
   Combobox.Dialog,
   Combobox.Filtering,
   Combobox.Navigation,
+  Combobox.NewOptions,
   Combobox.Options,
   Combobox.Selection,
   Combobox.Toggle,

app/assets/javascripts/hw_combobox/helpers.js
@@ -1,3 +1,5 @@
+export const nullEvent = new Event("NULL")
+
 export function Concerns(Base, ...mixins) {
   return mixins.reduce((accumulator, current) => current(accumulator), Base)
 }
@@ -49,4 +51,8 @@ export function debounce(fn, delay = 150) {
     clearTimeout(timeoutId)
     timeoutId = setTimeout(callback, delay)
   }
+}
+
+export function isDeleteEvent(event) {
+  return event.inputType === "deleteContentBackward" || event.inputType === "deleteWordBackward"
 }

app/assets/javascripts/hw_combobox/models/combobox.js
@@ -6,6 +6,7 @@ import "hw_combobox/models/combobox/autocomplete"
 import "hw_combobox/models/combobox/dialog"
 import "hw_combobox/models/combobox/filtering"
 import "hw_combobox/models/combobox/navigation"
+import "hw_combobox/models/combobox/new_options"
 import "hw_combobox/models/combobox/options"
 import "hw_combobox/models/combobox/selection"
 import "hw_combobox/models/combobox/toggle"

app/assets/javascripts/hw_combobox/models/combobox/autocomplete.js
@@ -29,11 +29,26 @@ Combobox.Autocomplete = Base => class extends Base {
     this.listboxTarget.style.display = "none"
   }
 
+  get _isExactAutocompleteMatch() {
+    return this._immediatelyAutocompletableValue === this._actingCombobox.value
+  }
+
+  // All `_isExactAutocompleteMatch` matches are `_isPartialAutocompleteMatch` matches
+  // but not all `_isPartialAutocompleteMatch` matches are `_isExactAutocompleteMatch` matches.
+  get _isPartialAutocompleteMatch() {
+    return !!this._immediatelyAutocompletableValue &&
+      startsWith(this._immediatelyAutocompletableValue, this._actingCombobox.value)
+  }
+
   get _autocompletesList() {
     return this.autocompleteValue === "both" || this.autocompleteValue === "list"
   }
 
   get _autocompletesInline() {
     return this.autocompleteValue === "both" || this.autocompleteValue === "inline"
+  }
+
+  get _immediatelyAutocompletableValue() {
+    return this._visibleOptionElements[0]?.getAttribute(this.autocompletableAttributeValue)
   }
 }

app/assets/javascripts/hw_combobox/models/combobox/filtering.js
@@ -1,6 +1,6 @@
 
 import Combobox from "hw_combobox/models/combobox/base"
-import { applyFilter, nextFrame, debounce } from "hw_combobox/helpers"
+import { applyFilter, nextFrame, debounce, isDeleteEvent } from "hw_combobox/helpers"
 import { get } from "hw_combobox/vendor/requestjs"
 
 Combobox.Filtering = Base => class extends Base {
@@ -25,7 +25,7 @@ Combobox.Filtering = Base => class extends Base {
 
     await get(this.asyncSrcValue, { responseKind: "turbo-stream", query: { q } })
 
-    this._afterTurboStreamRender(() => this._commitFilter(q, event))
+    this._afterTurboStreamRender(() => this._commitFilter(event))
   }
 
   _filterSync(event) {
@@ -35,15 +35,13 @@ Combobox.Filtering = Base => class extends Base {
 
     this._allOptionElements.forEach(applyFilter(query, { matching: this.filterableAttributeValue }))
 
-    this._commitFilter(query, event)
+    this._commitFilter(event)
   }
 
-  _commitFilter(query, event) {
-    const isDeleting = event.inputType === "deleteContentBackward"
-
-    if (this._isValidNewOption(query, { ignoreAutocomplete: isDeleting })) {
-      this._selectNew(query)
-    } else if (isDeleting) {
+  _commitFilter(event) {
+    if (this._shouldTreatAsNewOptionForFiltering(!isDeleteEvent(event))) {
+      this._selectNew()
+    } else if (isDeleteEvent(event)) {
       this._deselect()
     } else {
       this._select(this._visibleOptionElements[0])
@@ -53,5 +51,9 @@ Combobox.Filtering = Base => class extends Base {
   async _afterTurboStreamRender(callback) {
     await nextFrame()
     callback()
+  }
+
+  get _isQueried() {
+    return this._actingCombobox.value.length > 0
   }
 }

commit 095cc2f42092d9fc9f3d8bc67c6d144127aab353

Fix selection via enter key
Regressed in https://github.com/josefarias/hotwire_combobox/pull/37

app/assets/javascripts/hw_combobox/models/combobox/selection.js
@@ -71,9 +71,9 @@ Combobox.Selection = Base => class extends Base {
     }
   }
 
-  _selectFuzzyMatch() {
-    if (this._isFuzzyMatch) {
-      this._select(this._visibleOptionElements[0], { force: true })
+  _ensureSelection() {
+    if (this._shouldEnsureSelection) {
+      this._select(this._ensurableOption, { force: true })
       this.filter(nullEvent)
     }
   }
@@ -82,7 +82,11 @@ Combobox.Selection = Base => class extends Base {
     return this.hiddenFieldTarget.value && !this._selectedOptionElement
   }
 
-  get _isFuzzyMatch() {
-    return this._isQueried && !!this._visibleOptionElements[0] && !this._isNewOptionWithPotentialMatches
+  get _shouldEnsureSelection() {
+    return this._isQueried && !!this._ensurableOption && !this._isNewOptionWithPotentialMatches
+  }
+
+  get _ensurableOption() {
+    return this._selectedOptionElement || this._visibleOptionElements[0]
   }
 }

app/assets/javascripts/hw_combobox/models/combobox/toggle.js
@@ -50,10 +50,6 @@ Combobox.Toggle = Base => class extends Base {
     return clientX >= left && clientX <= right && clientY >= top && clientY <= bottom
   }
 
-  _ensureSelection() {
-    this._selectFuzzyMatch()
-  }
-
   _openByFocusing() {
     this._actingCombobox.focus()
   }

test/system/hotwire_combobox_test.rb
@@ -108,6 +108,19 @@ class HotwireComboboxTest < ApplicationSystemTestCase
     assert_selected_option_with selector: ".hw-combobox__option--selected", text: "Florida"
   end
 
+  test "selecting with the keyboard" do
+    visit html_options_path
+
+    open_combobox "#state-field"
+    type_in_combobox "#state-field", "mi"
+    type_in_combobox "#state-field", :down, :down
+    assert_selected_option_with text: "Mississippi"
+
+    type_in_combobox "#state-field", :enter
+    assert_closed_combobox
+    assert_combobox_display_and_value "#state-field", "Mississippi", "MS"
+  end
+
   test "pressing enter locks in the current selection, but editing the text field resets it" do
     visit html_options_path
 