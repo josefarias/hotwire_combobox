f48df85c43650ec27b6ad613c643d728c68fe091
Jose Farias <jose@37signals.com>
2024-04-07 19:17:42 -0600

    Fix states fixtures accessor now that Mocha overrides it

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,3 +1,8 @@
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome
+
+  def states(...)
+    # https://github.com/freerange/mocha/issues/620
+    access_fixture("states", ...)
+  end
 end
