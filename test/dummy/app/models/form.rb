dadf2ee4409e824d8f512912c494605eb6119a5a
Jose Farias <jose@farias.mx>
2024-04-21 13:14:47 -0600

    Use standard accessor for Form object

--- test/dummy/app/models/form.rb
+++ test/dummy/app/models/form.rb
@@ -2,9 +2,13 @@ class Form
   include ActiveModel::Model
   include ActiveModel::Attributes
 
-  attribute :state_id, :integer, default: State.first.id
+  attr_accessor :state_id
 
   def state
     State.find state_id
+  end
+
+  def state_id
+    @state_id || State.first.id
   end
 end
