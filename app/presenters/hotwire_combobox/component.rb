e7dd68bfe6b5799e5f537acb86ec2317a1f77b9d
Rei <chloerei@gmail.com>
2024-05-21 13:41:16 +0800

    fix name_when_new on multiselect with form scope name (#169)

--- app/presenters/hotwire_combobox/component.rb
+++ app/presenters/hotwire_combobox/component.rb
@@ -204,7 +204,7 @@ class HotwireCombobox::Component
     def name_when_new_on_multiselect_must_match_original_name
       return unless multiselect? && name_when_new.present?
 
-      unless name_when_new.to_s == name
+      unless name_when_new.to_s == hidden_field_name
         errors.add :name_when_new, :must_match_original_name,
           message: "must match the regular name ('#{name}', in this case) on multiselect comboboxes."
       end
