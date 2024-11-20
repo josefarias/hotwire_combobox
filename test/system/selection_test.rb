095ea65761576328ebfd8cb9831d9ee5d1ff76a2
Jose Farias <jose@farias.mx>
2024-11-19 19:04:44 -0600

    Sleep for flaky test

--- test/system/selection_test.rb
+++ test/system/selection_test.rb
@@ -147,6 +147,7 @@ class SelectionTest < ApplicationSystemTestCase
     assert_no_selector ".hw-combobox__input[data-queried]"
     open_combobox "#movie-field"
     click_on_option "Aladdin"
+    sleep 1
     assert_closed_combobox
     assert_combobox_display_and_value "#movie-field", "Aladdin", movies(:aladdin).id
     assert_selector ".hw-combobox__input[data-queried]"
