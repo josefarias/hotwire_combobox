d5524a55f59568f7bc451b1251b67adcd214ed7a
Jose Farias <jose@farias.mx>
2024-11-19 18:21:27 -0600

    Optionally preload async comboboxes

--- app/presenters/hotwire_combobox/component/paginated.rb
+++ app/presenters/hotwire_combobox/component/paginated.rb
@@ -4,6 +4,15 @@ module HotwireCombobox::Component::Paginated
   end
 
   def pagination_attrs
-    { for_id: canonical_id, src: async_src }
+    { for_id: canonical_id, src: async_src, loading: preload_next_page? ? :eager : :lazy }
   end
+
+  private
+    def preload_next_page?
+      view.hw_first_page? && preload?
+    end
+
+    def preload?
+      preload.present?
+    end
 end
