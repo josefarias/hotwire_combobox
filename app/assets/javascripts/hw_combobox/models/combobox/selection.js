5aa305cb1663765a647c40ef6e4541edbf7cba4b
Hans Lemuet <Spone@users.noreply.github.com>
2026-02-21 22:15:46 +0100

    Improve _ensurableOption getter logic

--- app/assets/javascripts/hw_combobox/models/combobox/selection.js
+++ app/assets/javascripts/hw_combobox/models/combobox/selection.js
@@ -150,6 +150,6 @@ Combobox.Selection = Base => class extends Base {
   }
 
   get _ensurableOption() {
-    return this._selectedOptionElement || this._visibleOptionElements[0]
+    return this._selectedOptionElement || this._optionElementWithValue(this._fieldValue) || this._visibleOptionElements[0]
   }
 }
